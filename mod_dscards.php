f33acc9e276a46fbcb5edb4479a62dd5a5371b05
Davide Strati - Graphic & Web Design <67890060+davide-strati@users.noreply.github.com>
2023-09-23 12:10:16 +0200

    xml and php update

--- mod_dscards.php
+++ mod_dscards.php
@@ -10,9 +10,13 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\ModuleHelper;
 use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\Module\DsCards\Site\Helper\DsCardsHelper;
+
+$wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+$wa->registerAndUseStyle('mod_dscards', 'modules/mod_dscards/tmpl/mod_dscards.css');
 
 // Get data from the model
 $cardsData = DsCardsHelper::getCardsData($params);
